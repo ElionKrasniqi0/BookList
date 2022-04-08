63a15a9cf3b32374ddc5314fef33a60fefc0dee5
ElionKrasniqi0 <96434456+ElionKrasniqi0@users.noreply.github.com>
2022-04-08 23:15:40 +0200

    .Net

---
+++ BookListRazor/Model/AplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class AplicationDbContext : Dbcontext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext>options) : base(options)
+        (
+            )
+          public DbSet<Book> Book { get; set; }
+    }
+}
